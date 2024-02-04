eaec41f75e164b9f1dd850c3fd11e51b8d08d3bc
barmud3 <barmud3@gmail.com>
2024-02-04 21:52:41 +0200

    add logout button in dash, secure dash page by changing URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,16 @@ import Login from "./pages/Login.jsx";
 import { Routes, Route } from "react-router-dom";
 import StudentDashTest from "./pages/StudentDashTest.jsx";
 import Signup from './pages/Signup.jsx'
+import PrivateRoute from "./components/PrivateRoute.jsx";
 // import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Layout></Layout>}> */}
-          <Route index element={<Login></Login>}></Route>
-          <Route path='/signup' element = {<Signup></Signup>}></Route>
-          {/* <Route path="/" element={<StudentDash />} /> */}
-          <Route path="/StudentDashTest" element={<StudentDashTest />} />
-        {/* </Route> */}
+          <Route index element={<Login/>} />
+          <Route path='/signup' element = {<Signup></Signup> }/>
+          <Route path="/StudentDashTest" element={<PrivateRoute><StudentDashTest /></PrivateRoute>}/>
       </Routes>
     </div>
   );
--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -3,35 +3,19 @@ import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { signOut } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../config/fire-base";
 
 const drawerWidth = 220;
 
 function ResponsiveDrawer(props) {
   const { window, user } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [isClosing, setIsClosing] = React.useState(false);
-
-  const handleDrawerClose = () => {
-    setIsClosing(true);
-    setMobileOpen(false);
-  };
-
-  const handleDrawerTransitionEnd = () => {
-    setIsClosing(false);
-  };
 
   const handleDrawerToggle = () => {
     if (!isClosing) {
@@ -39,40 +23,17 @@ function ResponsiveDrawer(props) {
     }
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+  const navigate = useNavigate();
 
-  // Remove this const when copying and pasting into your project.
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/"); // Redirect to the login page after logout
+      })
+      .catch((error) => {
+        console.error("Error during logout:", error.message);
+      });
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -97,6 +58,8 @@ function ResponsiveDrawer(props) {
           <Typography variant="h6" noWrap component="div">
           {user ? `Welcome, ${user.displayName}!` : 'Welcome!'}
           </Typography>
+          <Box sx={{ ml: 'auto' }} />
+          <button onClick={handleLogout}>Logout</button>
         </Toolbar>
       </AppBar>
     </Box>
