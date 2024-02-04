03ade9a38132f768fcd5898f33deba23c555b69b
Shachar96 <shachar.ketz@gmail.com>
2024-02-04 13:38:33 +0200

    stay in dash after reload the page, by checking the user auth

--- src/pages/StudentDashTest.jsx
+++ src/pages/StudentDashTest.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { auth, db, storage } from "../config/fire-base.jsx";
 // import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 // import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
-// import { signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+
 
 export default function StudentDashTest() {
   const navigate = useNavigate();
@@ -16,13 +17,15 @@ export default function StudentDashTest() {
   //     const [userPDFs, setUserPDFs] = useState([]);
 
   useEffect(() => {
-    // Redirect to login page if user is not authenticated
-    if (!user) {
-      navigate("/");
-    } else {
-      
-    }
-  }, [user, navigate]);
+    // Redirect to login page if user is not authenticated, stay in after reload the page
+    const unsubscribe = onAuthStateChanged(auth, (user)=>{
+      if(user){
+        navigate("/StudentDashTest");
+      }else{
+      }
+      return ()=> unsubscribe();
+    })
+  }, []);
 
   //   const logoutUser = async (e) => {
   //     e.preventDefault();
