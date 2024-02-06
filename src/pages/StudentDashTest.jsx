72811206e041d820eaf576be7e8aa6b974bd235e
Shachar96 <shachar.ketz@gmail.com>
2024-02-06 13:30:39 +0200

    get the user doc and extract him farther to Drower and TableTest

--- src/pages/StudentDashTest.jsx
+++ src/pages/StudentDashTest.jsx
@@ -3,13 +3,14 @@ import TableTest from "../components/TableTest";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db, storage } from "../config/fire-base.jsx";
+import { doc, getDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from "firebase/auth";
 
 
 export default function StudentDashTest() {
   const navigate = useNavigate();
-  const user = auth.currentUser;
-
+  const userAuth = auth.currentUser;
+  const [firebaseUser,setFirebaseUser] = useState(null);
 
   useEffect(() => {
     // Redirect to login page if user is not authenticated, stay in after refresh the page
@@ -17,17 +18,34 @@ export default function StudentDashTest() {
       if(user){
         console.log("User is signed in 1");
         navigate("/StudentDashTest");
+
+        //get user doc
+        const fetchData = async () => {
+          try {
+            const userRef = doc(db,"users",user.uid);
+            const userSnap = await getDoc(userRef);
+            const userRecord = userSnap.data();
+    
+            setFirebaseUser(userRecord);
+          } catch (error) {
+            console.error("Error fetching data from Firestore:", error);
+          }
+        };
+        fetchData();
       }else{
+        console.log("unsubscibed User");
       }
       return ()=> unsubscribe();
     })
   }, []);
 
+  
+
 
   return (
     <div>
-      <ResponsiveDrawer user={user} />
-      <TableTest user={user}/>
+      <ResponsiveDrawer firebaseUser={firebaseUser} />
+      <TableTest firebaseUser={firebaseUser}/>
     </div>
   );
 }
