3b4325c3b0d0b3297730a063993347fa261c5abc
Shachar96 <shachar.ketz@gmail.com>
2024-02-08 17:12:08 +0200

    not connected yet, will hold the user profile details

---
+++ src/components/Profile.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, db, storage } from "../config/fire-base.jsx";
+import { doc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from "firebase/auth";
+
+export default function Profile() {
+    const navigate = useNavigate();
+  const user = auth.currentUser;
+  const [firebaseUser,setFirebaseUser] = useState(null);
+
+  useEffect(() => {
+    // Redirect to login page if user is not authenticated, stay in after refresh the page
+    const unsubscribe = onAuthStateChanged(auth, (user)=>{
+      if(user){
+        console.log("User is signed in 1");
+        // navigate("/StudentDashTest");
+
+        //get user doc
+        const fetchData = async () => {
+          try {
+            const userRef = doc(db,"users",user.uid);
+            const userSnap = await getDoc(userRef);
+            const userRecord = userSnap.data();
+    
+            setFirebaseUser(userRecord);
+          } catch (error) {
+            console.error("Error fetching data from Firestore:", error);
+          }
+        };
+        fetchData();
+      }else{
+        console.log("unsubscibed User");
+      }
+      return ()=> unsubscribe();
+    })
+  }, []);
+
+    return(
+        <div>
+        {firebaseUser && (<>
+        <ResponsiveDrawer firebaseUser={firebaseUser} />
+        <div>
+          <h2>User Details:</h2>
+          <p>Name: {firebaseUser.name}</p>
+          <p>Email: {firebaseUser.email}</p>
+          <p>Address: {firebaseUser.address}</p>
+          <p>Phone: {firebaseUser.phone}</p>
+        </div>
+        {/* <TableTest firebaseUser={firebaseUser}/> */}
+        </>)
+        }
+      </div>
+    )
+}
