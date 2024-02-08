04eef7b497fa4086d590cae8fea0bd67122ec33b
Shachar96 <shachar.ketz@gmail.com>
2024-02-08 08:49:21 +0200

    desh give to the different tabels the user doc instade of auth details

--- src/components/TableChecker.jsx
+++ src/components/TableChecker.jsx
@@ -15,29 +15,29 @@
 
 
 
-  const TableChecker = ({ user }) => {
+  const TableChecker = ({ firebaseUser }) => {
 
     
-    const fetchUserId = async (db, userUid) => {
-      try {
-        // Query the 'users' collection where the 'id' field equals the user's UID
-        const querySnapshot = await getDocs(
-        query(collection(db, "users"), where("id", "==", userUid))
-      );
+  //   const fetchUserId = async (db, userUid) => {
+  //     try {
+  //       // Query the 'users' collection where the 'id' field equals the user's UID
+  //       const querySnapshot = await getDocs(
+  //       query(collection(db, "users"), where("id", "==", userUid))
+  //     );
 
-      // If a document is found, return its document ID
-      if (!querySnapshot.empty) {
-        const userId = querySnapshot.docs[0].id;
-        return userId;
-      } else {
-        // If no document is found, return null
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching user ID from Firestore:", error);
-      return null;
-    }
-  };
+  //     // If a document is found, return its document ID
+  //     if (!querySnapshot.empty) {
+  //       const userId = querySnapshot.docs[0].id;
+  //       return userId;
+  //     } else {
+  //       // If no document is found, return null
+  //       return null;
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching user ID from Firestore:", error);
+  //     return null;
+  //   }
+  // };
 
   const columns = [
     {field: "Student_ID", headerName: "Student_ID", width: 130 },
@@ -57,7 +57,7 @@
       renderCell: (value) => {
         const onDownload = async (row) => {
           try {
-            const userId = await fetchUserId(db, user.uid);
+            const userId = firebaseUser.id;
             const File_doc = row["File_doc"]; // Access the row object and get the value of "File_doc"
             console.log(File_doc);
             // Fetch all documents from the "pdfs" collection
@@ -96,7 +96,7 @@
               <VisibilityIcon />
             </IconButton>
             {/* Conditionally render the CreateAssignment component */}
-            {showReview && <Review user={user} onClose={() => setShowReview(false)} />}
+            {showReview && <Review firebaseUser={firebaseUser} onClose={() => setShowReview(false)} />}
           </div>
         );
       },
@@ -111,63 +111,63 @@
     const [selectedRowId, setSelectedRowId] = useState(null);
 
     useEffect(() => {
-    const fetchData = async () => {
-    try {
-      if (!user) {
-      console.log("User is not defined. Aborting data fetching.");
-      return;
-      }
+      const fetchData = async () => {
+        try {
+          if (!firebaseUser) {
+            console.log("User is not defined. Aborting data fetching.");
+            return;
+          }
 
-    // Fetch user ID
-    const userId = await fetchUserId(db, user.uid);
+          // Fetch user ID
+          const userId = firebaseUser.id
 
-    // Fetch user document
-    const userDoc = await getDoc(doc(db, "users", userId));
+          // Fetch user document
+          const userDoc = await getDoc(doc(db, "users", userId));
 
-    // Get the courses array from the user document
-    const userCourses = userDoc.data().courses || [];
+          // Get the courses array from the user document
+          const userCourses = userDoc.data().courses || [];
 
-    // Fetch assignments that match the user's courses
-    const assignmentsSnapshot = await getDocs(
-      query(collection(db, "assignments"), where("Course-ref", "in", userCourses))
-    );
+          // Fetch assignments that match the user's courses
+          const assignmentsSnapshot = await getDocs(
+            query(collection(db, "assignments"), where("Course-ref", "in", userCourses))
+          );
 
-    // Map the fetched assignments data
-      const data = await Promise.all(assignmentsSnapshot.docs.map(async (doc) => {
-      const assignmentData = doc.data();
-      
-      // Fetch corresponding user document based on the 'Owner' field
-      const userQuerySnapshot = await getDocs(query(collection(db, "users"), where("id", "==", assignmentData.Owner)));
-      
-      // Check if a matching user document exists
-      if (!userQuerySnapshot.empty) {
-        // Get the student_id from the user document
-        const studentId = userQuerySnapshot.docs[0].data().student_id;
-        // Return modified assignment data with the student_id
-        return {
-          id: doc.id,
-          ...assignmentData,
-          Student_ID: studentId // Add the Student_ID field to the assignment data
-        };
-      } else {
-        // If no matching user document found, return assignment data without modifying
-        return {
-          id: doc.id,
-          ...assignmentData
-        };
-      }
-    }));
+          // Map the fetched assignments data
+          const data = await Promise.all(assignmentsSnapshot.docs.map(async (doc) => {
+            const assignmentData = doc.data();
+            
+            // Fetch corresponding user document based on the 'Owner' field
+            const userQuerySnapshot = await getDocs(query(collection(db, "users"), where("id", "==", assignmentData.Owner)));
+            
+            // Check if a matching user document exists
+            if (!userQuerySnapshot.empty) {
+              // Get the student_id from the user document
+              const studentId = userQuerySnapshot.docs[0].data().student_id;
+              // Return modified assignment data with the student_id
+              return {
+                id: doc.id,
+                ...assignmentData,
+                Student_ID: studentId // Add the Student_ID field to the assignment data
+              };
+            } else {
+              // If no matching user document found, return assignment data without modifying
+              return {
+                id: doc.id,
+                ...assignmentData
+              };
+            }
+          }));
 
-    setRows(data);
-  } catch (error) {
-    console.error("Error fetching data from Firestore:", error);
-  }
-};
+          setRows(data);
+        } catch (error) {
+          console.error("Error fetching data from Firestore:", error);
+        }
+      };
 
 
       console.log("Starting data fetching process...");
       fetchData();
-    }, [user]);
+  }, [firebaseUser]);
 
     const handleUploadOpen = (rowId) => {
       setSelectedRowId(rowId);
--- src/pages/CheckerDash.jsx
+++ src/pages/CheckerDash.jsx
@@ -3,20 +3,34 @@ import TableChecker from "../components/TableChecker.jsx";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db, storage } from "../config/fire-base.jsx";
+import { doc, getDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from "firebase/auth";
 
 
 export default function ChckerDash() {
   const navigate = useNavigate();
   const user = auth.currentUser;
+  const [firebaseUser,setFirebaseUser] = useState(null);
 
 
   useEffect(() => {
     // Redirect to login page if user is not authenticated, stay in after refresh the page
     const unsubscribe = onAuthStateChanged(auth, (user)=>{
       if(user){
-        navigate("/CheckerDash");
+        const fetchData = async () => {
+        try {
+          const userRef = doc(db,"users",user.uid);
+          const userSnap = await getDoc(userRef);
+          const userRecord = userSnap.data();
+  
+          setFirebaseUser(userRecord);
+        } catch (error) {
+          console.error("Error fetching data from Firestore:", error);
+        }
+      };
+      fetchData();
       }else{
+        console.log("unsubscibed User");
       }
       return ()=> unsubscribe();
     })
@@ -25,8 +39,8 @@ export default function ChckerDash() {
 
   return (
     <div>
-      {<ResponsiveDrawer user={user} />}
-      {<TableChecker user={user}/>}
+      {<ResponsiveDrawer firebaseUser={firebaseUser} />}
+      {<TableChecker firebaseUser={firebaseUser}/>}
     </div>
   );
 }
