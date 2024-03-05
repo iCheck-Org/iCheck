93acc72be6c4a76106ca11e7e516aeb7ca7531c0
Dor Shir <Dorshir92@gmail.com>
2024-03-05 14:57:22 +0200

    Acc menu

--- src/components/NavigateBar/AccountMenu.jsx
+++ src/components/NavigateBar/AccountMenu.jsx
@@ -1,34 +1,31 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Logout from "@mui/icons-material/Logout";
 import { auth } from "../../config/fire-base";
-import Collapse from "@mui/material/Collapse";
 import { signOut } from "firebase/auth";
+import "../../pages/styles.css";
+import Typography from "@mui/material/Typography";
 
 export default function AccountMenu(props) {
   const { firebaseUser } = props;
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [profileOpen, setProfileOpen] = React.useState(false);
+  const [isIconButtonClicked, setIsIconButtonClicked] = React.useState(false);
 
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+    setIsIconButtonClicked(true);
   };
 
   const handleClose = () => {
     setAnchorEl(null);
-    setProfileOpen(false); // Close profile details when menu is closed
-  };
-
-  const handleProfileClick = () => {
-    setProfileOpen(!profileOpen); // Toggle profile details visibility
+    setIsIconButtonClicked(false);
   };
 
   const handleLogout = () => {
@@ -45,51 +42,29 @@ export default function AccountMenu(props) {
     <React.Fragment>
       <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
         <Tooltip>
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Avatar sx={{ width: 39, height: 39, bgcolor: "#fffdf1", color: "#374785" }}>
+          <IconButton onClick={handleClick} size="small">
+            <Avatar
+              sx={{
+                width: 38,
+                height: 38,
+                bgcolor: isIconButtonClicked
+                  ? "rgbargb(182, 213, 230, 0.001)"
+                  : "#f0f0f0",
+                color: "#374785",
+              }}
+            >
               {firebaseUser.name[0]}{" "}
             </Avatar>
           </IconButton>
         </Tooltip>
       </Box>
+
       <Menu
         anchorEl={anchorEl}
         id="account-menu"
         open={open}
         onClose={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&::before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
+        className="menuStyle"
         anchorOrigin={{
           vertical: "bottom",
           horizontal: "right",
@@ -98,48 +73,51 @@ export default function AccountMenu(props) {
           vertical: "top",
           horizontal: "right",
         }}
+
       >
-        <MenuItem
-          onClick={handleProfileClick}
-          sx={{
-            backgroundColor: profileOpen ? "#e3f2fd" : "transparent", 
-          }}
-        >
-          <Avatar /> Profile
-        </MenuItem>
-        <Collapse
-          in={profileOpen}
-          timeout="auto"
-          unmountOnExit
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-        >
-          <MenuItem onClick={handleClose} sx={{ fontSize: "small" }}>
-            Name: {firebaseUser.name}
-          </MenuItem>
-          <MenuItem onClick={handleClose} sx={{ fontSize: "small" }}>
-            ID: {firebaseUser.personal_id}
-          </MenuItem>
-          <MenuItem onClick={handleClose} sx={{ fontSize: "small" }}>
-            Email: {firebaseUser.email}{" "}
-          </MenuItem>
-          <MenuItem onClick={handleClose} sx={{ fontSize: "small" }}>
-            Type: {firebaseUser.type}
-          </MenuItem>
-        </Collapse>
-        <Divider />
-        <MenuItem onClick={handleLogout} sx={{ color: "#ff5a4d" }}>
-          <ListItemIcon sx={{ color: "#ee6868" }}>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
+        <div className="menuContent">
+          <div className="menuBackground" />
+          <Avatar sx={{ color: "#374785", bgcolor: "#f0f0f0" }}>
+            <Typography fontSize={"x-large"}>{firebaseUser.name[0]}</Typography>
+          </Avatar>
+          <Typography
+            sx={{ color: "#494f4e", fontSize: "15px", fontWeight: "bold" }}
+          >
+            {firebaseUser.name}
+          </Typography>
+          <Typography
+            sx={{ marginTop: "-8px", color: "#494f4e", fontSize: "12px" }}
+          >
+            {firebaseUser.email}
+          </Typography>
+          <Box
+            sx={{
+              display: "inline-block",
+              padding: "1px 6px",
+              borderRadius: "5px",
+              backgroundColor: "#B6D5E6",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "small",
+                textTransform: "capitalize",
+                color: "#494f4e",
+              }}
+            >
+              {firebaseUser.type}
+            </Typography>
+          </Box>
+          <Divider className="dividerStyle" sx={{ marginTop: "16px" }} />
+          <IconButton
+            onClick={handleLogout}
+            sx={{ color: "#494f4e", marginTop: "5px" }}
+          >
+            <Tooltip title="Logout" enterDelay={"300"}>
+              <Logout fontSize="small" />
+            </Tooltip>
+          </IconButton>
+        </div>
       </Menu>
     </React.Fragment>
   );
